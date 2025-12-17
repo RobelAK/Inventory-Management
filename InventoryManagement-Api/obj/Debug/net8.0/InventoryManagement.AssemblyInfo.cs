203dbf8166a8a9542717f8b5d69eb1b20729d1b1
robelak <robelaklilu100@gmail.com>
2025-12-17 18:22:51 +0300

    updated-loginpage

--- InventoryManagement-Api/obj/Debug/net8.0/InventoryManagement.AssemblyInfo.cs
+++ InventoryManagement-Api/obj/Debug/net8.0/InventoryManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c4d650f673d69733046fcf2df6a3a61c61b93b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f32b891524f6142bc0f902eeb8617bf325bf8991")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
